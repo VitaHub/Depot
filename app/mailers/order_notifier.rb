bda7a2a3962bb50b9cfe7699626c78789a8aac2d
VitaHub <hubenko_vitalii@mail.ua>
2016-09-22 00:28:11 +0300

    Email delivery and complex testing

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ApplicationMailer
+
+  default from: 'Vitalii Hubenko <depot@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'Подтверждение заказа'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Заказ отправлен'
+  end
+end
